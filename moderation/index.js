d4f034089965e21a3d1e51bd3e5a47606a3df2d2
Yan P Aung <yan.pye.aung@gmail.com>
2021-11-22 20:11:48 -0800

    Emit CommentModerated event and also emit other events to moreation service

--- moderation/index.js
+++ moderation/index.js
@@ -2,10 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 
+const badWord = 'orange';
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {});
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'CommentCreated') {
+    const status = data.content.include(badWord) ? 'rejected' : 'approved';
+
+    await axios
+      .post('http://localhost:4005/events', {
+        type: 'CommentModerated',
+        data: {
+          id: data.id,
+          postId: data.postId,
+          status,
+          content: data.content,
+        },
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    res.send({});
+  }
+});
 
 app.listen(4003, () => {
   console.log('Listening on 4003');
