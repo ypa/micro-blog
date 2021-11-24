6e908af834b09b814f80866612ce84528f70449d
Yan P Aung <yan.pye.aung@gmail.com>
2021-11-23 19:41:16 -0800

    Handle comment moderated event

--- comments/index.js
+++ comments/index.js
@@ -44,6 +44,33 @@ app.post('/posts/:id/comments', async (req, res) => {
 app.post('/events', (req, res) => {
   console.log('Received Event', req.body.type);
 
+  const { type, data } = req.body;
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+
+    await axios
+      .post('http://localhost:4005', {
+        type: 'CommentUpdated',
+        data: {
+          id,
+          status,
+          postId,
+          content,
+        },
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   res.send({});
 });
 
