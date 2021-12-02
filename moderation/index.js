c9ea3ab49ae5b2a7e586ec317f5b1391ad2468f3
Yan P Aung <yan.pye.aung@gmail.com>
2021-12-01 19:51:09 -0800

    Update the rest of the services to commnuicate with event-bus-srv

--- moderation/index.js
+++ moderation/index.js
@@ -13,7 +13,7 @@ app.post('/events', async (req, res) => {
     const status = data.content.includes(badWord) ? 'rejected' : 'approved';
 
     await axios
-      .post('http://localhost:4005/events', {
+      .post('http://event-bus-srv:4005/events', {
         type: 'CommentModerated',
         data: {
           id: data.id,
